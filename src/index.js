f97c022b21fcb26eb87defe9ebed5cebd3728adb
pelongngoan <long.tl446@gmail.com>
2023-10-22 22:28:31 +0700

    c

--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import TicTacToe from './TicTacToe';
-import reportWebVitals from './reportWebVitals';
+import FilterTable from './FilterTable';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <TicTacToe />
+    <FilterTable />
   </React.StrictMode>
   //dkjfskjfsdkj
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
